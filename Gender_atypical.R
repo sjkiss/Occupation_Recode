9c02cbc3ae0b3ceab27b6c7c7f50187b7627caea
rafceegee <35352704+rafceegee@users.noreply.github.com>
2026-06-15 12:57:55 -0400

    updated the occupations

--- Gender_atypical.R
+++ Gender_atypical.R
@@ -16,13 +16,13 @@ library(nnet)
 library(cesdata2)
 
 ##### Marriage and Occupation ####
-female_atypical <- c(
+female_atypical <- c("13",
   "70","80","90","213","22","421","72","73","74","75",
   "82","83","84","85","92","93"
 )
 
 male_atypical <- c(
-  "13","313","32","33","422","44","45"
+  "313","32","33","422","441","45", "131"
 )
 
 ces21 <- ces21 %>%
@@ -32,6 +32,13 @@ ces21 <- ces21 %>%
       cps21_genderid == 1 &
         str_starts(NOC_chr, str_c("^(", str_c(male_atypical, collapse = "|"), ")")) ~ 1,
       cps21_genderid == 2 &
+        str_starts(NOC_chr, str_c("^(", str_c(female_atypical, collapse = "|"), ")")) ~ 1,
+      TRUE ~ 0
+    ),
+    gender_stereotypical = case_when(
+      cps21_genderid == 2 &
+        str_starts(NOC_chr, str_c("^(", str_c(male_atypical, collapse = "|"), ")")) ~ 1,
+      cps21_genderid == 1 &
         str_starts(NOC_chr, str_c("^(", str_c(female_atypical, collapse = "|"), ")")) ~ 1,
       TRUE ~ 0
     )
@@ -53,6 +60,13 @@ ces19web <- ces19web %>%
       cps19_gender == 1 &
         str_starts(NOC_chr, str_c("^(", str_c(male_atypical, collapse = "|"), ")")) ~ 1,
       cps19_gender == 2 &
+        str_starts(NOC_chr, str_c("^(", str_c(female_atypical, collapse = "|"), ")")) ~ 1,
+      TRUE ~ 0
+    ),
+    gender_stereotypical = case_when(
+      cps19_gender == 2 &
+        str_starts(NOC_chr, str_c("^(", str_c(male_atypical, collapse = "|"), ")")) ~ 1,
+      cps19_gender == 1 &
         str_starts(NOC_chr, str_c("^(", str_c(female_atypical, collapse = "|"), ")")) ~ 1,
       TRUE ~ 0
     )
@@ -155,7 +169,97 @@ interaction_df %>%
   theme(legend.position = "bottom") + 
   scale_colour_manual(values = c("darkblue", "orange")) +
   geom_vline(xintercept = 0, lty = 4, col = "grey30") +
-  labs(x = "Marginal Effect of Being Employed in a Gender Atypical Role on Marriage Prospects by Gender",
+  labs(x = "Marginal Effect of Being Employed in a Gender Atypical Role \n on Marriage Prospects by Gender",
        y = NULL) + 
   guides(color = guide_legend(reverse = TRUE))
 
+
+#### Gender Stereotypical ####
+
+
+base_stereo_19 <- lm(Married ~ as.factor(gender_stereotypical) + cps19_gender, ces19_straight) 
+base_stereo_21 <- lm(Married ~ as.factor(gender_stereotypical) + cps21_genderid, ces21_straight) 
+
+
+base_stereo_controls_19 <- lm(Married ~ as.factor(gender_stereotypical) + cps19_gender  + as.factor(income) + degree + age, ces19_straight) 
+base_stereo_controls_21 <- lm(Married ~ as.factor(gender_stereotypical) + cps21_genderid  + as.factor(income) + degree + age, ces21_straight) 
+
+base_stereo_19_df <- tidy(base_stereo_19, conf.int = TRUE) %>% 
+  mutate(Year = 2019)
+base_stereo_21_df <- tidy(base_stereo_21, conf.int = TRUE) %>% 
+  mutate(Year = 2021)
+base_stereo_controls_19_df <- tidy(base_stereo_controls_19, conf.int = TRUE) %>% 
+  mutate(Year = 2019)
+base_stereo_controls_21_df <- tidy(base_stereo_controls_21, conf.int = TRUE) %>% 
+  mutate(Year = 2021)
+
+base_stereo_models <- rbind(base_stereo_19_df %>% mutate(Controls = "Gender Only"),
+                     base_stereo_21_df %>% mutate(Controls = "Gender Only"),
+                     base_stereo_controls_19_df %>% mutate(Controls = "Demographic Controls"),
+                     base_stereo_controls_21_df %>% mutate(Controls = "Demographic Controls"))
+
+#### Graph the Base Model ####
+
+base_stereo_models %>% 
+  filter(term == "as.factor(gender_stereotypical)1") %>%
+  mutate(term = replace(term, term == "as.factor(gender_stereotypical)1", "Employed in a Gender \n Stereotypical Occupation")) %>% 
+  ggplot(aes(x = estimate, xmin = conf.low, xmax = conf.high, col = Controls, y = term)) +
+  geom_point(position = position_dodge(width = 0.6)) + 
+  geom_linerange(position = position_dodge(width = 0.6)) + 
+  facet_wrap(~Year) + 
+  theme_bw() +
+  theme(legend.position = "bottom") + 
+  scale_colour_manual(values = c("darkblue", "orange")) +
+  geom_vline(xintercept = 0, lty = 4, col = "grey30") +
+  labs(x = "OLS Coefficent Estimate and 95% Confidence Intervals",
+       y = NULL) + 
+  guides(color = guide_legend(reverse = TRUE))
+
+#### Gender Interaction Model ####
+
+interact_stereo_19 <- lm(Married ~ as.factor(gender_stereotypical) * cps19_gender, ces19_straight) 
+interact_stereo_21 <- lm(Married ~ as.factor(gender_stereotypical) * cps21_genderid, ces21_straight) 
+
+interact_stereo_19_df <- avg_slopes(interact_stereo_19, variables = "gender_stereotypical", by = "cps19_gender") %>% 
+  mutate(Controls = "No Controls",
+         Year = 2019)
+interact_stereo_21_df <- avg_slopes(interact_stereo_21, variables = "gender_stereotypical", by = "cps21_genderid") %>% 
+  mutate(Controls = "No Controls",
+         Year = 2021)  %>% 
+  rename(cps19_gender = cps21_genderid)
+
+interact_stereo_control_19 <- lm(Married ~ as.factor(gender_stereotypical) * cps19_gender + as.factor(income) + degree + age, ces19_straight) 
+interact_stereo_control_21 <- lm(Married ~ as.factor(gender_stereotypical) * cps21_genderid + as.factor(income) + degree + age, ces21_straight) 
+
+
+interact_stereo_control_19_df <- avg_slopes(interact_stereo_control_19, variables = "gender_stereotypical", by = "cps19_gender") %>% 
+  mutate(Controls = "Demographic Controls",
+         Year = 2019) 
+interact_stereo_control_21_df <- avg_slopes(interact_stereo_control_21, variables = "gender_stereotypical", by = "cps21_genderid") %>% 
+  mutate(Controls = "Demographic Controls",
+         Year = 2021) %>% 
+  rename(cps19_gender = cps21_genderid)
+
+intereact_stereo_df <- rbind(interact_stereo_19_df, 
+                        interact_stereo_21_df,
+                        interact_stereo_control_19_df,
+                        interact_stereo_control_21_df
+) %>% 
+  as.data.frame()
+
+#### Interaction Plot ####
+
+interaction_df %>% 
+  mutate(Gender = case_when(cps19_gender == 1 ~ "Man",
+                            cps19_gender == 2 ~ "Woman")) %>% 
+  ggplot(aes(x = estimate, xmin = conf.low, xmax = conf.high, col = Controls, y = Gender)) +
+  geom_point(position = position_dodge(width = 0.6)) + 
+  geom_linerange(position = position_dodge(width = 0.6)) + 
+  theme_bw() +
+  facet_wrap(~Year) + 
+  theme(legend.position = "bottom") + 
+  scale_colour_manual(values = c("darkblue", "orange")) +
+  geom_vline(xintercept = 0, lty = 4, col = "grey30") +
+  labs(x = "Marginal Effect of Being Employed in a Gender Stereotypical Role \n on Marriage Prospects by Gender",
+       y = NULL) + 
+  guides(color = guide_legend(reverse = TRUE))
